4b46789763228642223d168c71de256dd2edf441
Anton Rybak <toxa-wow@mail.ru>
2022-09-22 19:39:12 +0300

    homework done

---
+++ task54/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.Clear();
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+int[,] matrix = FillArray(rows, columns, 1, 10);
+PrintArray(matrix);
+Console.WriteLine();
+ArraySort(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+void ArraySort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int n = 0; n < array.GetLength(1) - 1; n++)
+            {
+                if (array[i, n] < array[i, n + 1])
+                {
+                    int temp = array[i, n + 1];
+                    array[i, n + 1] = array[i, n];
+                    array[i, n] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] FillArray(int rows, int columns, int min, int max)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] filledArray)
+{
+    for (int i = 0; i < filledArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < filledArray.GetLength(1); j++)
+        {
+            Console.Write($"{filledArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
